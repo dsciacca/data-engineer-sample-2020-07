27326c6f8920f93a0a4cf35abd6beb68a96b0683
dsciacca <dsciacca@regis.edu>
2021-08-13 17:47:45 -0600

    Updated main.py w/ solution

--- main.py
+++ main.py
@@ -2,9 +2,29 @@
 This is the entrypoint to the program. 'python main.py' will be executed and the 
 expected csv file should exist in ../data/destination/ after the execution is complete.
 """
-
+from src.some_storage_library import SomeStorageLibrary
 
 
 if __name__ == '__main__':
     """Entrypoint"""
     print('Beginning the ETL process...')
+    data_path = "data/source/SOURCEDATA.txt"
+    schema_path = "data/source/SOURCECOLUMNS.txt"
+    output_file = 'output.csv'
+    storage_library = SomeStorageLibrary()
+
+    with open(data_path, 'r') as f:
+        print(f"Reading {data_path}...")
+        data = f.read().replace('|', ',')
+    with open(schema_path, 'r') as f:
+        print(f"Reading {schema_path}...")
+        schema = f.read().split('\n')
+
+    schema = [(x.split('|')[0], x.split('|')[1]) for x in schema]
+    schema.sort(key=lambda x: int(x[0]))
+    header = ','.join([x[1] for x in schema])
+
+    with open('output.csv', 'w') as f:
+        f.write(f"{header}\n{data}")
+
+    storage_library.load_csv('output.csv')
